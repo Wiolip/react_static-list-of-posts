08bea5333a71438b2ede89a30eff2ecbc7e6be2a
Wioleta Lip <wiolip@hotmail.com>
2026-01-30 23:56:36 +0100

    develop

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,3 +1,35 @@
 import React from 'react';
+import { type Post } from '../../types/Post';
+import { UserInfo } from '../UserInfo';
+import { CommentList } from '../CommentList';
+import './PostInfo.css';
 
-export const PostInfo: React.FC = () => <>Put the post here</>;
+type Props = {
+  post: Post;
+};
+
+export const PostInfo: React.FC<Props> = ({ post }) => {
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+
+        <p>
+          {' Posted by  '}
+
+          {post.user ? <UserInfo user={post.user} /> : null}
+        </p>
+      </div>
+
+      <p className="PostInfo__body">{post.body}</p>
+
+      <hr />
+
+      {post.comments.length === 0 ? (
+        <b data-cy="NoCommentsMessage">No comments yet</b>
+      ) : (
+        <CommentList comments={post.comments} />
+      )}
+    </div>
+  );
+};
--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,3 +1,18 @@
 import React from 'react';
+import { PostInfo } from '../PostInfo';
+import { type Post } from '../../types/Post';
+import './PostList.css';
 
-export const PostList: React.FC = () => <>Put the list here</>;
+type Props = {
+  posts: Post[];
+};
+
+export const PostList: React.FC<Props> = ({ posts }) => {
+  return (
+    <div className="PostList">
+      {posts.map(post => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </div>
+  );
+};
