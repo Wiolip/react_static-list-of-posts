7aa49daebbc6a616fded0049507882e05fe0f9ab
Wioleta Lip <wiolip@hotmail.com>
2026-01-31 00:07:49 +0100

    solution

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -9,26 +9,28 @@ type Props = {
 };
 
 export const PostInfo: React.FC<Props> = ({ post }) => {
+  const hasComments = post.comments && post.comments.length > 0;
+
   return (
     <div className="PostInfo">
       <div className="PostInfo__header">
         <h3 className="PostInfo__title">{post.title}</h3>
 
-        <p>
-          {' Posted by  '}
-
-          {post.user ? <UserInfo user={post.user} /> : null}
-        </p>
+        {post.user && (
+          <p>
+            Posted by <UserInfo user={post.user} />
+          </p>
+        )}
       </div>
 
       <p className="PostInfo__body">{post.body}</p>
 
       <hr />
 
-      {post.comments.length === 0 ? (
+      {!hasComments ? (
         <b data-cy="NoCommentsMessage">No comments yet</b>
       ) : (
-        <CommentList comments={post.comments} />
+        <CommentList comments={post.comments!} />
       )}
     </div>
   );
